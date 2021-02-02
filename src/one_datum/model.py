21f7be7567a5d4994cffe0b5aff8773037c9bd58
Dan F-M <foreman.mackey@gmail.com>
2021-02-02 11:40:51 -0500

    adding uncertainty grid

--- src/one_datum/model.py
+++ src/one_datum/model.py
@@ -1,54 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__all__ = [
-    "get_uncertainty_model",
-    "RVErrorModel",
-    "DR2RVErrorModel",
-    "BasicDR2RVErrorModel",
-]
+__all__ = ["RVErrorModel", "DR2RVErrorModel", "BasicDR2RVErrorModel"]
 
-from typing import Callable, Iterable, Optional, Tuple, Union
+from typing import Optional, Tuple
 
 import kepler
 import numpy as np
-import pkg_resources
 import scipy.stats
-from astropy.io import fits
-from scipy.interpolate import RegularGridInterpolator
-from scipy.ndimage import gaussian_filter
-
-
-def get_uncertainty_model(
-    *, bounds_error: bool = False, fill_value: Optional[float] = None
-) -> RegularGridInterpolator:
-    filename = pkg_resources.resource_filename(
-        __name__, "data/rv_uncertainty_grid.fits"
-    )
-
-    with fits.open(filename) as f:
-        hdr = f[0].header
-        mu = f[1].data
-
-    color_bins = np.linspace(
-        hdr["MIN_COL"], hdr["MAX_COL"], hdr["NUM_COL"] + 1
-    )
-    mag_bins = np.linspace(hdr["MIN_MAG"], hdr["MAX_MAG"], hdr["NUM_MAG"] + 1)
-
-    ln_sigma_model = gaussian_filter(np.mean(mu, axis=-1), (1, 0.8))
-
-    return RegularGridInterpolator(
-        [
-            0.5 * (mag_bins[1:] + mag_bins[:-1]),
-            0.5 * (color_bins[1:] + color_bins[:-1]),
-        ],
-        ln_sigma_model,
-        bounds_error=bounds_error,
-        fill_value=fill_value,
-    )
-
-
-Shape = Union[int, Iterable[int]]
-Distribution = Callable[[Shape], np.ndarray]
 
 
 class RVErrorModel:
