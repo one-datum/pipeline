adding a figure and fixing minus signs

workflow/scripts/infer_noise.py
@@ -57,7 +57,7 @@ def setup_model() -> SVI:
         )
         sigma2 = jnp.exp(2 * log_sigma0) + jnp.exp(2 * log_dsigma)
         stat = sample_variance * (num_transit - 1) / sigma2
-        numpyro.sample("obs", dist.Chi2(num_transit - 1), obs=stat)
+        numpyro.sample("obs", dist.Chi2(num_transit + 1), obs=stat)
 
     def guide(num_transit, sample_variance):
         mu_log_sigma0 = numpyro.param(