d9000a1ba8e2725a4e0bcde9607a040a02e03304
Dan Foreman-Mackey <danfm@nyu.edu>
2021-07-05 13:19:15 -0400

    switching to adaptive grid w plx

--- workflow/scripts/noise/infer.py
+++ workflow/scripts/noise/infer.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from functools import partial
 from typing import Tuple
 
+import jax
 import jax.numpy as jnp
 import numpy as np
 import numpyro
 import numpyro.distributions as dist
 from astropy.io import fits
 from jax import random
-from jax.config import config
+from jax.config import config as jax_config
+from jax.interpreters import xla
 from numpyro.infer import SVI, Trace_ELBO
 from tqdm import tqdm
 
-config.update("jax_enable_x64", True)
+jax_config.update("jax_enable_x64", True)
 
 
 MIN_COLOR: float = 0.0
@@ -23,44 +26,43 @@ MAX_MAG: float = 16.0
 
 
 def setup_model() -> SVI:
-    def model(num_transit, sample_variance):
-        log_sigma0 = numpyro.sample("log_sigma0", dist.Normal(0.0, 10.0))
+    def model(num_transit, log_plx, stat=None):
+        m = numpyro.sample("m", dist.Normal(0.0, 10.0))
+        b = numpyro.sample("b", dist.Normal(0.0, 10.0))
+        log_sigma0 = numpyro.deterministic("log_sigma0", m * log_plx + b)
         log_dsigma = numpyro.sample(
             "log_dsigma",
             dist.Normal(0.0, 10.0),
-            sample_shape=(len(sample_variance),),
+            sample_shape=(len(num_transit),),
         )
         sigma2 = jnp.exp(2 * log_sigma0) + jnp.exp(2 * log_dsigma)
-        stat = sample_variance * (num_transit - 1)
         numpyro.sample(
             "obs", dist.Gamma(0.5 * (num_transit - 1), 0.5 / sigma2), obs=stat
         )
 
-    def guide(num_transit, sample_variance):
-        mu_log_sigma0 = numpyro.param(
-            "mu_log_sigma0", 0.5 * np.log(np.median(sample_variance))
-        )
-        sigma_log_sigma0 = numpyro.param(
-            "sigma_log_sigma0", 1.0, constraint=dist.constraints.positive
-        )
-
-        mu_log_dsigma = numpyro.param(
-            "mu_log_dsigma", 0.5 * np.log(sample_variance)
-        )
-        sigma_log_dsigma = numpyro.param(
-            "sigma_log_dsigma",
-            np.ones_like(sample_variance),
+    def make_mean_field(name, init):
+        mu = numpyro.param(f"mu_{name}", init)
+        sigma = numpyro.param(
+            f"sigma_{name}",
+            1.0 + 0.0 * init,
             constraint=dist.constraints.positive,
         )
+        return numpyro.sample(name, dist.Normal(mu, sigma))
 
-        numpyro.sample(
-            "log_sigma0", dist.Normal(mu_log_sigma0, sigma_log_sigma0)
-        )
-        numpyro.sample(
-            "log_dsigma", dist.Normal(mu_log_dsigma, sigma_log_dsigma)
+    def guide(num_transit, log_plx, stat=None):
+        numpyro.param("m", 0.0)
+        numpyro.param("b", 0.0)
+        make_mean_field(
+            "log_dsigma",
+            0.5
+            * jnp.log(
+                jnp.ones_like(log_plx)
+                if stat is None
+                else (stat / (num_transit - 1))
+            ),
         )
 
-    optimizer = numpyro.optim.Adam(step_size=0.05)
+    optimizer = numpyro.optim.Adam(step_size=2e-1)
     return SVI(model, guide, optimizer, loss=Trace_ELBO())
 
 
@@ -68,8 +70,6 @@ def load_data(
     data_path: str,
     *,
     min_nb_transits: int = 3,
-    color_range: Tuple[float, float] = (MIN_COLOR, MAX_COLOR),
-    mag_range: Tuple[float, float] = (MIN_MAG, MAX_MAG),
 ) -> fits.fitsrec.FITS_rec:
     print("Loading data...")
     with fits.open(data_path) as f:
@@ -78,15 +78,29 @@ def load_data(
     m = np.isfinite(data["phot_g_mean_mag"])
     m &= np.isfinite(data["bp_rp"])
     m &= np.isfinite(data["dr2_radial_velocity_error"])
+    m &= np.isfinite(data["parallax"])
+    m &= data["parallax"] > 0
 
     m &= data["dr2_rv_nb_transits"] > min_nb_transits
 
-    m &= color_range[0] < data["bp_rp"]
-    m &= data["bp_rp"] < color_range[1]
-    m &= mag_range[0] < data["phot_g_mean_mag"]
-    m &= data["phot_g_mean_mag"] < mag_range[1]
+    # Remove colors and manitudes with 1e+20 entries
+    m &= data["phot_g_mean_mag"] < 100
+    m &= data["bp_rp"] < 100
 
     return data[m]
+
+
+@partial(jax.jit, static_argnums=(0, 1))
+def train(svi, num_optim, key, num_transit, log_plx, stat):
+    params, _ = svi.run(
+        key,
+        num_optim,
+        num_transit,
+        log_plx,
+        stat,
+        progress_bar=False,
+    )
+    return params["b"], params["m"]
 
 
 def fit_data(
@@ -94,13 +108,9 @@ def fit_data(
     *,
     num_mag_bins: int,
     num_color_bins: int,
-    color_range: Tuple[float, float] = (MIN_COLOR, MAX_COLOR),
-    mag_range: Tuple[float, float] = (MIN_MAG, MAX_MAG),
-    num_iter: int = 5,
-    targets_per_fit: int = 1000,
-    num_optim: int = 5000,
+    num_optim: int = 1000,
     seed: int = 11239,
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Tuple[np.ndarray, np.ndarray]]:
     # Parse data
     num_transit = np.ascontiguousarray(
         data["dr2_rv_nb_transits"], dtype=np.int32
@@ -109,6 +119,8 @@ def fit_data(
         data["dr2_radial_velocity_error"], dtype=np.float32
     )
     sample_variance = 2 * num_transit * (eps ** 2 - 0.11 ** 2) / np.pi
+    stat = sample_variance * (num_transit - 1)
+    log_plx = np.log(data["parallax"])
     mag = np.ascontiguousarray(data["phot_g_mean_mag"], dtype=np.float32)
     color = np.ascontiguousarray(data["bp_rp"], dtype=np.float32)
 
@@ -116,16 +128,16 @@ def fit_data(
     svi = setup_model()
 
     # Setup the grid and allocate the memory
-    mag_bins = np.linspace(mag_range[0], mag_range[1], num_mag_bins + 1)
-    color_bins = np.linspace(
-        color_range[0], color_range[1], num_color_bins + 1
+    color_bins = np.percentile(
+        data["bp_rp"], np.linspace(0, 100, num_color_bins + 1)
     )
-    mu = np.empty((len(mag_bins) - 1, len(color_bins) - 1, num_iter))
-    sigma = np.empty_like(mu)
-    count = np.empty((len(mag_bins) - 1, len(color_bins) - 1), dtype=np.int64)
+    mag_bins = np.percentile(
+        data["phot_g_mean_mag"], np.linspace(0, 100, num_mag_bins + 1)
+    )
+    intercept = np.full((len(mag_bins) - 1, len(color_bins) - 1), np.nan)
+    slope = np.full((len(mag_bins) - 1, len(color_bins) - 1), np.nan)
+    count = np.zeros((len(mag_bins) - 1, len(color_bins) - 1), dtype=np.int64)
 
-    np.random.seed(seed)
-    inds = np.arange(len(data))
     for n in tqdm(range(len(mag_bins) - 1), desc="magnitudes"):
         for m in tqdm(range(len(color_bins) - 1), desc="colors", leave=False):
             mask = mag_bins[n] <= mag
@@ -134,44 +146,22 @@ def fit_data(
             mask &= color <= color_bins[m + 1]
 
             count[n, m] = mask.sum()
-
-            # For small amounts of data
-            if count[n, m] <= targets_per_fit:
-                if count[n, m] < 50:
-                    mu[n, m, :] = np.nan
-                    sigma[n, m, :] = np.nan
-                    continue
-
-                svi_result = svi.run(
-                    random.PRNGKey(seed + n + m),
-                    num_optim,
-                    num_transit[mask],
-                    sample_variance[mask],
-                    progress_bar=False,
-                )
-                params = svi_result.params
-                mu[n, m, :] = params["mu_log_sigma0"]
-                sigma[n, m, :] = params["sigma_log_sigma0"]
+            if count[n, m] <= 1:
                 continue
 
-            for k in tqdm(range(num_iter), desc="iterations", leave=False):
-                masked_inds = np.random.choice(
-                    inds[mask],
-                    size=targets_per_fit,
-                    replace=mask.sum() <= targets_per_fit,
-                )
-                svi_result = svi.run(
-                    random.PRNGKey(seed + n + m + k),
-                    num_optim,
-                    num_transit[masked_inds],
-                    sample_variance[masked_inds],
-                    progress_bar=False,
-                )
-                params = svi_result.params
-                mu[n, m, k] = params["mu_log_sigma0"]
-                sigma[n, m, k] = params["sigma_log_sigma0"]
+            intercept[n, m], slope[n, m] = train(
+                svi,
+                num_optim,
+                random.PRNGKey(seed + n + m),
+                num_transit[mask],
+                log_plx[mask],
+                stat[mask],
+            )
 
-    return mu, sigma, count
+        # Try not to have JAX memory footprint grow forever
+        xla._xla_callable.cache_clear()
+
+    return intercept, slope, count, (mag_bins, color_bins)
 
 
 if __name__ == "__main__":
@@ -189,32 +179,13 @@ if __name__ == "__main__":
         config = yaml.load(f.read(), Loader=yaml.FullLoader)
 
     data = load_data(
-        data_path=args.input,
-        min_nb_transits=config["min_nb_transits"],
-        color_range=(
-            config["min_color"],
-            config["max_color"],
-        ),
-        mag_range=(
-            config["min_mag"],
-            config["max_mag"],
-        ),
+        data_path=args.input, min_nb_transits=config["min_nb_transits"]
     )
 
-    mu, sigma, count = fit_data(
+    intercept, slope, count, bins = fit_data(
         data,
         num_mag_bins=config["num_mag"],
         num_color_bins=config["num_color"],
-        color_range=(
-            config["min_color"],
-            config["max_color"],
-        ),
-        mag_range=(
-            config["min_mag"],
-            config["max_mag"],
-        ),
-        num_iter=config["num_iter"],
-        targets_per_fit=config["targets_per_fit"],
         num_optim=config["num_optim"],
         seed=config["seed"],
     )
@@ -222,21 +193,17 @@ if __name__ == "__main__":
     # Save the results
     hdr = fits.Header()
     hdr["min_tra"] = config["min_nb_transits"]
-    hdr["min_col"] = config["min_color"]
-    hdr["max_col"] = config["max_color"]
     hdr["num_col"] = config["num_color"]
-    hdr["min_mag"] = config["min_mag"]
-    hdr["max_mag"] = config["max_mag"]
     hdr["num_mag"] = config["num_mag"]
-    hdr["num_itr"] = config["num_iter"]
-    hdr["num_per"] = config["targets_per_fit"]
     hdr["num_opt"] = config["num_optim"]
     hdr["seed"] = config["seed"]
     fits.HDUList(
         [
             fits.PrimaryHDU(header=hdr),
-            fits.ImageHDU(mu),
-            fits.ImageHDU(sigma),
+            fits.ImageHDU(intercept),
+            fits.ImageHDU(slope),
             fits.ImageHDU(count),
+            fits.ImageHDU(bins[0], name="mag bins"),
+            fits.ImageHDU(bins[1], name="color bins"),
         ]
     ).writeto(args.output, overwrite=True)
