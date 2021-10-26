421056113f11c27b8ce905bffbfaba31b4fa6587
Dan Foreman-Mackey <danfm@nyu.edu>
2021-10-26 14:45:35 -0400

    switch to numpyro-ncx2

--- workflow/scripts/noise/infer.py
+++ workflow/scripts/noise/infer.py
@@ -4,23 +4,15 @@
 from typing import Tuple
 
 import jax.numpy as jnp
-import jax.scipy as jsp
 import numpy as np
 import numpyro
 import numpyro.distributions as dist
-import tensorflow_probability.substrates.jax as tfp
 from astropy.io import fits
-from jax import lax, random
+from jax import random
 from jax.config import config as jax_config
-from numpyro.distributions import constraints
-from numpyro.distributions.distribution import Distribution
 from numpyro.distributions.transforms import AffineTransform
-from numpyro.distributions.util import (
-    is_prng_key,
-    promote_shapes,
-    validate_sample,
-)
 from numpyro.infer import SVI
+from numpyro_ncx2 import NoncentralChi2
 from tqdm import tqdm
 
 jax_config.update("jax_enable_x64", True)
@@ -30,71 +22,6 @@ MIN_COLOR: float = 0.0
 MAX_COLOR: float = 5.5
 MIN_MAG: float = 4.5
 MAX_MAG: float = 16.0
-
-
-def _random_chi2(key, df, shape=(), dtype=jnp.float_):
-    return 2.0 * random.gamma(key, 0.5 * df, shape=shape, dtype=dtype)
-
-
-class NoncentralChi2(Distribution):
-    arg_constraints = {
-        "df": constraints.positive,
-        "nc": constraints.positive,
-    }
-    support = constraints.positive
-    reparametrized_params = ["df", "nc"]
-
-    def __init__(self, df, nc, validate_args=None):
-        self.df, self.nc = promote_shapes(df, nc)
-        batch_shape = lax.broadcast_shapes(jnp.shape(df), jnp.shape(nc))
-        super(NoncentralChi2, self).__init__(
-            batch_shape=batch_shape, validate_args=validate_args
-        )
-
-    def sample(self, key, sample_shape=()):
-        # Ref: https://github.com/numpy/numpy/blob/
-        #      89c80ba606f4346f8df2a31cfcc0e967045a68ed/numpy/random/src/
-        #      distributions/distributions.c#L797-L813
-        assert is_prng_key(key)
-        shape = sample_shape + self.batch_shape + self.event_shape
-
-        key1, key2, key3 = random.split(key, 3)
-
-        i = random.poisson(key1, 0.5 * self.nc, shape=shape)
-        n = random.normal(key2, shape=shape) + jnp.sqrt(self.nc)
-        cond = jnp.greater(self.df, 1.0)
-        chi2 = _random_chi2(
-            key3,
-            jnp.where(cond, self.df - 1.0, self.df + 2.0 * i),
-            shape=shape,
-        )
-        return jnp.where(cond, chi2 + n * n, chi2)
-
-    @validate_sample
-    def log_prob(self, value):
-        # Ref: https://github.com/scipy/scipy/blob/
-        #      500878e88eacddc7edba93dda7d9ee5f784e50e6/scipy/stats/
-        #      _distn_infrastructure.py#L597-L610
-        df2 = self.df / 2.0 - 1.0
-        xs, ns = jnp.sqrt(value), jnp.sqrt(self.nc)
-        res = (
-            jsp.special.xlogy(df2 / 2.0, value / self.nc)
-            - 0.5 * (xs - ns) ** 2
-        )
-        corr = tfp.math.bessel_ive(df2, xs * ns) / 2.0
-        return jnp.where(
-            jnp.greater(corr, 0.0),
-            res + jnp.log(corr),
-            -jnp.inf,
-        )
-
-    @property
-    def mean(self):
-        return self.df + self.nc
-
-    @property
-    def variance(self):
-        return 2.0 * (self.df + 2.0 * self.nc)
 
 
 def setup_model(sample_variance) -> SVI:
